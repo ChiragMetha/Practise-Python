03d3073ade981b49120fe64246af092490b3b99b
chirag metha <chiragmetha1805@gmail.com>
2024-01-15 16:56:34 +0530

    Kilometer to Miles

---
+++ kilometersToMiles.py
@@ -0,0 +1,5 @@
+kilometer = int(input("Enter the Kilometers u travel "))
+# The conversion factor for kilometer to miles is 0.621371
+con_factor = 0.621371
+miles  = kilometer * con_factor
+print(f"{kilometer} is equal to {miles}")
