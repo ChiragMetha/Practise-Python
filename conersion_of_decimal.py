7f05454f665263c1c7bc5ddd11c91c5c6dd8a0c3
chirag metha <chiragmetha1805@gmail.com>
2024-01-19 15:00:27 +0530

    Converion of decimal into binary oct hexa

---
+++ conersion_of_decimal.py
@@ -0,0 +1,4 @@
+dec = int(input("Enter a decimal number "))
+print(f"conversion of decimal into binary is {bin(dec)}")
+print(f"conversion of decimal into hexagonal is {hex(dec)}")
+print(f"conversion of decimal into octagonal is {oct(dec)}")
