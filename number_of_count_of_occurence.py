4e57a76874ba020a66abaaf8c2d63fda6f2a208e
chirag metha <chiragmetha1805@gmail.com>
2024-01-25 16:31:50 +0530

    number of count of occurence of same alphabet in string

---
+++ number_of_count_of_occurence.py
@@ -0,0 +1,13 @@
+# using for loop
+count = 0
+my_str = "Chirag Metha"
+char = "a"
+for i in my_str:
+    if i == char:
+        count += 1
+print(count)
+
+# using count function
+
+my_str = "Chirag Metha"
+print(my_str.count('a'))
