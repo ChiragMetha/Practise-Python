22592e1c917bb8468e1b04490300210d68924608
chirag metha <chiragmetha1805@gmail.com>
2024-02-02 12:59:20 +0530

    Index accessing

---
+++ list1.py
@@ -0,0 +1,12 @@
+# list Iteration
+list = [4,5,8,64,24,20]
+for i in list:
+    print(i)
+
+# Create an array of 5 integers and display the array items
+list1 = [1,3,5,7,9]
+for i in list1:
+    print(i)
+#accesing first 3 elements from array
+print("First three elements are ")
+print(list1[0:3])
